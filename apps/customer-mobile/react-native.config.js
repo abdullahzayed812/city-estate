96ea5e09bd385f3b143659c6e910f72fb12f824b
abdullah <aabdullah.zayed0099@gmail.com>
2026-07-09 03:32:01 +0300

    Untrack mobile apps from git
    broker-mobile and customer-mobile are built/distributed as APKs, not
    deployed from the repo, so they don't need to live in the GitHub history.

--- apps/customer-mobile/react-native.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  project: {
-    android: { sourceDir: './android' },
-    ios: { sourceDir: './ios' },
-  },
-  assets: ['./assets'],
-  commands: require('@bam.tech/react-native-make/dist/rn-plugin.config').rnPluginConfig.commands,
-};
